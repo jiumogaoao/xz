1bdd21175c76f9b715d1a3be084702e1d2124820
jiumogaoao <394127821@qq.com>
2015-07-21 07:28:55 +0800

    change
    change

--- control/index.js
+++ control/index.js
@@ -3,9 +3,11 @@
 	obj.set({
 		name:"index",
 		par:"",
-		tem:["template_head","template_foot"],
+		tem:["template_head","template_foot","banner","template_center_two","deal_list"],
 		fn:function(data){
 			$("#head").html(data.tem[0]);
+			$("#middle").html(data.tem[2]+data.tem[3]);
+			$(".template_left").html(data.tem[4]);
 			$("#foot").html(data.tem[1]);
 			}
 		});
